43993c5c952ca5d1370f6babbf970a90f9e2b568
David Celis <david@davidcelis.com>
2012-08-03 11:18:42 -0700

    Add pre version for @creativetags to use in his PR.
    Signed-off-by: David Celis <david@davidcelis.com>

--- lib/inflections/version.rb
+++ lib/inflections/version.rb
@@ -2,6 +2,7 @@ module Inflections
   MAJOR = 3
   MINOR = 2
   PATCH = 7
+  PRE   = 20120803
 
-  VERSION = [MAJOR, MINOR, PATCH].compact.join '.'
+  VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join '.'
 end
