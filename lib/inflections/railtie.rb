a2f09d1f130284526041d02d6bfa6f2d9527c7c1
David Celis <me@davidcel.is>
2013-01-14 18:41:22 -0600

    Fix bad require for a nil default_locale
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/inflections/railtie.rb
+++ lib/inflections/railtie.rb
@@ -1,13 +1,15 @@
 module Inflections
   class Railtie < Rails::Railtie
-    begin
-      config.to_prepare do
+    config.to_prepare do
+      begin
         require "inflections/#{Rails.configuration.i18n.default_locale}"
-      end
-    rescue LoadError => e
-      config.to_prepare do
-        warn "Inflections currently does not support #{Rails.configuration.i18n.default_locale}. Defaulting to English (en)."
-        require 'inflections/en'
+      rescue LoadError => e
+        if Rails.configuration.i18n.default_locale.nil?
+          warn "Inflections: no default locale set. Defaulting to English (en)."
+        else
+          warn "Inflections: no support for #{Rails.configuration.i18n.default_locale}. Defaulting to English (en)."
+          require 'inflections/en'
+        end
       end
     end
   end
