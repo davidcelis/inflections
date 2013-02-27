68649fa5e3ec59f1beaa4104d80d2208dad8db66
David Celis <me@davidcel.is>
2013-02-27 12:29:23 -0800

    Build on 2.0.0 for Travis; don't build on 1.9.2 anymore
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/inflections/version.rb
+++ lib/inflections/version.rb
@@ -2,7 +2,7 @@ module Inflections
   MAJOR = 4
   MINOR = 0
   PATCH = 0
-  PRE   = 'beta'
+  PRE   = 'beta1'
 
-  VERSION = [MAJOR, MINOR, PATCH].compact.join '.'
+  VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join '.'
 end
