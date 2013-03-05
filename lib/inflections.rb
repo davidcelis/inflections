bed3ff46f37d1a91e61efb1dddf78ac25c3dbd08
David Celis <me@davidcel.is>
2013-03-05 09:29:59 -0800

    Fix #9 - only require the railtie.
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/inflections.rb
+++ lib/inflections.rb
@@ -1,8 +1,1 @@
 require 'inflections/railtie' if defined?(Rails)
-
-require 'inflections/en'
-require 'inflections/en-GB'
-require 'inflections/es'
-require 'inflections/kk'
-require 'inflections/nb'
-require 'inflections/tr'
