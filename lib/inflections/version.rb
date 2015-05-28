b13ef21e9c3fc5874cf87df0343f208344fc8c19
David Celis <me@davidcel.is>
2015-05-28 10:32:09 -0700

    Bump version to 4.0.1
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/inflections/version.rb
+++ lib/inflections/version.rb
@@ -1,8 +1,7 @@
 module Inflections
   MAJOR = 4
   MINOR = 0
-  PATCH = 0
-  PRE   = nil
+  PATCH = 1
 
-  VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join '.'
+  VERSION = [MAJOR, MINOR, PATCH].compact.join '.'
 end
